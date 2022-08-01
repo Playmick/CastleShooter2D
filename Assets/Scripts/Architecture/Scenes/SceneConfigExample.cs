35c3a2e7d067dacf9956a60c2950f399d2ba950e
Playmick <maltsev.a.a.1.4m@gmail.com>
2022-08-02 04:09:32 +0500

    Architecture level 3.1

--- Assets/Scripts/Architecture/Scenes/SceneConfigExample.cs
+++ Assets/Scripts/Architecture/Scenes/SceneConfigExample.cs
@@ -13,7 +13,11 @@ namespace Architecture.Scenes
             var repositoriesMap = new Dictionary<Type, Repository>();
 
             this.CreateRepository<BankRepository>(repositoriesMap);
-            //this.CreateRepository<BankRepository>(repositoriesMap);
+            //тут мы можем создавать множество таких же репозиториев, просто сейчас он существует только один
+            //(Банковский репозиторий хранящий денюжки)
+            //поэтому я множу код
+            //в будущем нужно сделать репозиторий по каждый навык
+            //this.CreateRepository<HealthRepository>(repositoriesMap);
             //this.CreateRepository<BankRepository>(repositoriesMap);
             //this.CreateRepository<BankRepository>(repositoriesMap);
 
@@ -25,9 +29,16 @@ namespace Architecture.Scenes
             var interactorsMap = new Dictionary<Type, Interactor>();
 
             this.CreateInteractor<BankInteractor>(interactorsMap);
+            //тут мы можем создавать множество таких же интеракторов, просто сейчас он существует только один
+            //(Банковский интерактор работающий с банком)
+            //поэтому я множу код
+            //в будущем нужно сделать интерактор по каждый навык
             //this.CreateInteractor<BankInteractor>(interactorsMap);
             //this.CreateInteractor<BankInteractor>(interactorsMap);
             //this.CreateInteractor<BankInteractor>(interactorsMap);
+
+            //а ещё сцену с главным меню в котором нужен репозиторий текстов и текущего языка
+            //репозиторий текстов будет грузить тексты для кнопок из ScriptableObjects
 
             return interactorsMap;
         }
